dcaa8cf49f3076a94ef40c89066ec8bc1a1870f8
JaGz2107 <proyectagz@gmail.com>
2023-07-12 13:12:02 -0300

    Se agrego

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "./public")))
+
+app.listen(3030, () => console.log('APP corriendo en el puerto:' + 3030))
+
+app.get('/', (req, res) => {
+    res.sendfile(path.resolve(__dirname, "./views/home.html"))
+})
