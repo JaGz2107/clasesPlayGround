e353ccf89e3141b55c18f7046996026356ad3357
JaGz2107 <proyectagz@gmail.com>
2023-07-30 19:27:28 -0300

    Formularios

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ app.listen(3030, () => console.log('APP corriendo en el puerto:' + 3030))
 app.get('/', (req, res) => {
     res.sendfile(path.resolve(__dirname, "./views/home.html"))
 })
+app.get('/registrar', (req, res) =>{
+    res.sendfile(path.resolve(__dirname, "./views/register.html"))
+})
+app.get('/ingresa', (req, res) =>{
+    res.sendfile(path.resolve(__dirname, "./views/ingresa.html"))
+})
